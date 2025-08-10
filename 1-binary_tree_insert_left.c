cf384f79b033d38d607841137c7bc2bf3dcd12c6
tranbnn <tranbn@outlook.com>
2025-08-10 02:45:49 +0000

    1-binary_tree_insert_left.c

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - inserts a node as the left child of another node
+ * @parent: the parent of the new node
+ * @value: the value to store in the new node
+ * Return: pointer to the created  node or NULL
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+}
