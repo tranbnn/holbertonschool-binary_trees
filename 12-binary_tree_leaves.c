930d5c404b7f6c3e44ee8c78674633f60896061b
tranbnn <tranbn@outlook.com>
2025-08-10 10:50:51 +0000

    12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,4 +8,21 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	size_t count;
+	
+	count = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		count += 1;
+	}
+
+	count += binary_tree_leaves(tree->left);
+	count += binary_tree_leaves(tree->right);
+	return (count);
 }
