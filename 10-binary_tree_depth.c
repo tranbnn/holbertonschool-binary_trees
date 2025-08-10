f27afa6d11bd0ecdbc3a4e948c1c26edc7bc1fa9
tranbnn <tranbn@outlook.com>
2025-08-10 10:37:55 +0000

    10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,4 +8,20 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+	size_t depth;
+
+	depth = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	while (tree->parent != NULL)
+	{
+		tree = tree->parent;
+		depth++;
+	}
+
+	return (depth);
 }
