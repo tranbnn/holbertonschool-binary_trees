a6cb222892958c2db93b817df0c2c669f7218589
tranbnn <tranbn@outlook.com>
2025-08-10 10:06:24 +0000

    7

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -9,4 +9,12 @@
 
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
+	if (tree == NULL || func == NULL)
+	{
+		return;
+	}
+
+	binary_tree_inorder(tree->left, func);
+	func(tree->);
+	binary_tree_inorder(tree->right, func);
 }
