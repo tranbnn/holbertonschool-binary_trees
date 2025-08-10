4ef0278d776986a5421a4e447329f78a99a9dc41
tranbnn <tranbn@outlook.com>
2025-08-10 10:29:55 +0000

    9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,4 +8,31 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t countleft;
+	size_t countright;
+
+	countleft = 0;
+	countright = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (0);
+	}
+
+	countleft = binary_tree_height(tree->left);
+	countright = binary_tree_height(tree->right);
+
+	if (countleft >= countright)
+	{
+		return (countleft + 1);
+	}
+	else
+	{
+		return (countright + 1);
+	}
 }
