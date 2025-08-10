c37e92840c849b235e3327b1e8115d7635cd85f0
tranbnn <tranbn@outlook.com>
2025-08-10 09:39:27 +0000

    4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -13,12 +13,11 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 		return (0);
 	}
 
-	if (node->left == NULL || node->right == NULL)
+	binary_tree_is_leaf(node->left);
+	if (node->left == NULL && node->right == NULL)
 	{
 		return (1);
 	}
-	else
-	{
-		return (0);
-	}
+	binary_tree_is_leaf(node->right);
+	return (0);
 }
