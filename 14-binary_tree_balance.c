e9b6dceb6251b9c7e191b61d8889a804345a6b8b
tranbnn <tranbn@outlook.com>
2025-08-31 10:50:35 +0000

    balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,4 +8,55 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	size_t left_height;
+	size_t right_height;
+
+	left_height = 0;
+	right_height = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+	return (left_height - right_height);
+}
+
+/**
+ * binary_tree_height - measures height of a binary tree
+ * @tree: pointer to the root of the tree
+ * Return: size_t or 0
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t countleft;
+	size_t countright;
+
+	countleft = 0;
+	countright = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (0);
+	}
+
+	countleft = binary_tree_height(tree->left);
+	countright = binary_tree_height(tree->right);
+
+	if (countleft >= countright)
+	{
+		return (countleft + 1);
+	}
+	else
+	{
+		return (countright + 1);
+	}
 }
